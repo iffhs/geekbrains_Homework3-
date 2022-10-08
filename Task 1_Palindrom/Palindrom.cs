00f65ed47a2e04fbe8dfcca11ddaa1443f38039a
Egor <kosterev2k17@yandex.ru>
2022-10-08 20:21:47 +0300

    Hello 2

---
+++ Task 1_Palindrom/Palindrom.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] Count = new int[number.ToString().Length];
+if (Count.Length == 5)
+{
+    int k = 0;
+    while (number > 0)
+    {
+            Count[k] = number % 10;
+            number = number / 10;
+            k++;
+    }
+    if (Count[0] == Count[4] && Count[1] == Count[3])
+        Console.WriteLine("Число является палиндромом");
+    else
+        Console.WriteLine("Число не является палиндромом");
+}
+else
+{
+    return;
+}
+
+
+
---
+++ Task2_3D/3D.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+const int X = 0;
+const int Y = 1;
+const int Z = 2;
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] InputPoint(int point)
+{
+    int[] answer = new int[3];
+    answer[X] = Prompt ($"Введите x {point} -->");
+    answer[Y] = Prompt ($"Введите y {point} -->");
+    answer[Z] = Prompt ($"Введите z {point} -->");
+    return answer;
+}
+int[] p1 = InputPoint(1);
+int[] p2 = InputPoint(2);
+
+double lenght = Math.Sqrt(Math.Pow(p2[X] - p1[X], 2) + Math.Pow(p2[Y] - p1[Y], 2) + Math.Pow(p2[Z] - p1[Z], 2));
+System.Console.WriteLine($"{lenght:f2}");
